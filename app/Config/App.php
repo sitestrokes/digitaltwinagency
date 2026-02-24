160949db5795111b4272b7f608bea2940fc135ac
Soham Banik <sohamb@OnlightenMedia.onmicrosoft.com>
2026-02-24 18:53:49 +0530

    Update app baseURL and DB credentials
    Switch baseURL from localhost to https://twinprofits.engageleads.net/ (commented out the local URL) and populate the Database config with connection details: set username, password, and database name. This prepares the app to use the target domain and configured MySQL database.

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8181/';
+    // public string $baseURL = 'http://localhost:8181/';
+    public string $baseURL = 'https://twinprofits.engageleads.net/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
